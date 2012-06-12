716cd1ca8c9a57e108abf39f196fe711c78a8763
Vincent Schüßler <vincent@vschuessler.org>
2012-06-12 22:54:02 +0200

    Precalculate sample_* data

--- perlin/perlin.lua
+++ perlin/perlin.lua
@@ -29,24 +29,43 @@ function perlin_reuse(perlinNoise, width, height, octaves, persistance)
 		end
 	end
 
+	local sample_x0 = {}
+	local sample_x1 = {}
+	local sample_y0 = {}
+	local sample_y1 = {}
+	local horizontal_blend = {}
+	local vertical_blend = {}
+	for oct = 1, octaves do
+		local samplePeriod = 2^oct
+		local sampleFrequency = 1 / samplePeriod
+		table.insert(sample_x0, {})
+		table.insert(sample_x1, {})
+		table.insert(sample_y0, {})
+		table.insert(sample_y1, {})
+		table.insert(horizontal_blend, {})
+		table.insert(vertical_blend, {})
+		for x = 0, #perlinNoise-1 do
+			table.insert(sample_x0[oct], math.floor(x / samplePeriod) * samplePeriod)
+			table.insert(sample_x1[oct], (sample_x0[oct][x+1] + samplePeriod) % #noise)
+			table.insert(horizontal_blend[oct], (x - sample_x0[oct][x+1]) * sampleFrequency)
+		end
+		for y = 0, #perlinNoise[1]-1 do
+			table.insert(sample_y0[oct], math.floor(y /samplePeriod) * samplePeriod)
+			table.insert(sample_y1[oct], (sample_y0[oct][y+1] + samplePeriod) % #noise[1])
+			table.insert(vertical_blend[oct], (y - sample_y0[oct][y+1]) * sampleFrequency)
+		end
+	end
+
 	local amplitude = 1.0
 	local totalAmplitude = 0.0
-	for octave = octaves, 1, -1 do
+	for oct = octaves, 1, -1 do
 		amplitude = amplitude * persistance
 		totalAmplitude = totalAmplitude + amplitude
-		local samplePeriod = 2^octave
-		local sampleFrequency = 1 / samplePeriod
-		for x = 0, #perlinNoise-1 do
-			local sample_x0 = math.floor(x / samplePeriod) * samplePeriod
-			local sample_x1 = (sample_x0 + samplePeriod) % #noise
-			local horizontal_blend = (x - sample_x0) * sampleFrequency
-			for y = 0, #perlinNoise[x+1]-1 do
-				local sample_y0 = math.floor(y /samplePeriod) * samplePeriod
-				local sample_y1 = (sample_y0 + samplePeriod) % #noise[x+1]
-				local vertical_blend = (y - sample_y0) * sampleFrequency
-				local top = interpolate(noise[sample_x0+1][sample_y0+1], noise[sample_x1+1][sample_y0+1], horizontal_blend)
-				local bottom = interpolate(noise[sample_x0+1][sample_y1+1], noise[sample_x1+1][sample_y1+1], horizontal_blend)
-				perlinNoise[x+1][y+1] = perlinNoise[x+1][y+1] + interpolate(top, bottom, vertical_blend) * amplitude
+		for x = 1, #perlinNoise do
+			for y = 1, #perlinNoise[x] do
+				local top = interpolate(noise[sample_x0[oct][x]+1][sample_y0[oct][y]+1], noise[sample_x1[oct][x]+1][sample_y0[oct][y]+1], horizontal_blend[oct][x])
+				local bottom = interpolate(noise[sample_x0[oct][x]+1][sample_y1[oct][y]+1], noise[sample_x1[oct][x]+1][sample_y1[oct][y]+1], horizontal_blend[oct][x])
+				perlinNoise[x][y] = perlinNoise[x][y] + interpolate(top, bottom, vertical_blend[oct][y]) * amplitude
 			end
 		end
 	end
